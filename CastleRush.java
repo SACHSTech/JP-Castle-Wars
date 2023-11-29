c9e498beadc474eb1b474e9adbe7207b9025a66e
Jacky-Ho0429 <jacky.ho25@ycdsbk12.ca>
2023-11-28 21:39:45 -0500

    Formatted + Clickable Enemies

--- CastleRush.java
+++ CastleRush.java
@@ -11,37 +11,50 @@ public class CastleRush extends PApplet {
 
     int castleHealth;
     int wave = 1;
+    int spawnedEnemies = 0;
+    int gold = 100;
     
     ArrayList<Enemy> enemies = new ArrayList<>();
 
     public static void main(String[] args) {
+
         PApplet.main("CastleRush");
+
     }
 
     public void settings() {
+
         size(800, 600);
+
     }
 
     public void setup() {
+
         castleImage = loadImage("Maps/Castle.png");
         castleHealth = 100;
         enemies = new ArrayList<>();
+
     }
 
     public void draw() {
+
         background(255);
         image(castleImage, width - castleImage.width, height / 2 - castleImage.height / 2);
-        drawHealthGold(width - 150, 20, castleHealth, 100, 100);
+        drawHealthGold(width - 150, 20, castleHealth, 100);
         spawnEnemy();
         updateEnemies();
         drawEnemies();
 
         if (lostGame()) {
+
             exit();
+
         }
+
     }
 
-    public void drawHealthGold(float x, float y, float health, float maxHealth, float gold) {
+    public void drawHealthGold(float x, float y, float health, float maxHealth) {
+        
         float barWidth = 100;
         float barHeight = 10;
 
@@ -57,72 +70,134 @@ public class CastleRush extends PApplet {
     }
 
     public void spawnEnemy() {
+
         int randomY = random.nextInt(568 - 32 + 1) + 32;
+
         if (frameCount % 60 == 0) {
+
             int x = 0;
             Enemy enemy = new Enemy(x, randomY);
             enemy.moveTowards(670, 300);
             enemies.add(enemy);
+            spawnedEnemies++;
+
+            if (spawnedEnemies % 8 == 0) {
+
+                wave ++;
+
+            }
+            
         }
+
     }
 
     public void updateEnemies() {
+
         for (int i = enemies.size() - 1; i >= 0; i--) {
+
             Enemy enemy = enemies.get(i);
             enemy.moveTowards(670, 300);
 
             if (enemy.isAtTarget(670, 300)) {
+
                 castleHealth -= enemy.getHealth();
                 enemies.remove(i);
+
             }
+
+            if (mouseX >= enemy.x - 10 && mouseX <= enemy.x + 10 && mouseY >= enemy.y - 10 && mouseY <= enemy.y + 10 && mousePressed) {
+
+                if (enemy.getHealth() <= 0) {
+
+                    enemies.remove(i);
+
+                } else {
+
+                    enemy.health -= 1;
+
+                }
+
+                gold += enemy.getMaxHealth();
+
+            }
+
         }
+
     }
 
     public boolean lostGame() {
+
         return castleHealth <= 0;
+
     }
 
     public void drawEnemies() {
+
         for (Enemy enemy : enemies) {
+
             fill(0, 255, 0);
             ellipse(enemy.x, enemy.y, 20, 20);
+
         }
+
     }
 
     public class Enemy {
+
         int x;
         int y;
         int health;
+        int maxHealth;
 
-        Enemy(int x, int y) {
+        public Enemy(int x, int y) {
+
             this.x = x;
             this.y = y;
-            this.health = 1 + wave;
+            this.health = wave;
+            this.maxHealth = wave;
+
         }
 
-        void moveTowards(int targetX, int targetY) {
+        public void moveTowards(int targetX, int targetY) {
+
             float distance = dist(x, y, targetX, targetY);
             float speed = wave;
 
             if (distance < 400) {
+
                 if (x < targetX) x += speed;
                 else if (x > targetX) x -= speed;
 
                 if (y < targetY) y += speed;
                 else if (y > targetY) y -= speed;
+
             } else {
+                
                 if (x < targetX) x += speed;
                 else if (x > targetX) x -= speed;
             }
+
         }
 
-        boolean isAtTarget(int targetX, int targetY) {
+        public boolean isAtTarget(int targetX, int targetY) {
+
             float threshold = 5;
             return dist(x, y, targetX, targetY) < threshold;
+
         }
 
-        int getHealth() {
+        public int getHealth() {
+
             return health;
+
         }
+
+        public int getMaxHealth() {
+
+            return maxHealth;
+
+        }
+
     }
+
 }
